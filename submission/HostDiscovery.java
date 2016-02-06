f2403c853b9c0b89864ad46602733fa8ec15943c
Vivekanandan, Karthik (kvivek) <kvivekanandan@stubhub.com>
2016-02-06 12:19:31 -0700

    ssh config

--- submission/HostDiscovery.java
+++ submission/HostDiscovery.java
@@ -9,24 +9,27 @@ import java.io.IOException;
  */
 
 public class HostDiscovery {
-	public static boolean readFile(String filename) throws FileNotFoundException {
+	public static boolean readFileEtcHosts(String filename) throws FileNotFoundException {
 		BufferedReader br;
 		try {
-			br = new BufferedReader(new FileReader(new File(filename)));
-			String line = null;
-			while ((line = br.readLine()) != null) {
-				if(!line.isEmpty() && !line.startsWith("#")){
-					String t = line.trim().replaceAll("\\s+"," ");
-					if(t.contains("#")){
-						t = t.split("#")[0];
+			File f = new File(filename);
+			if (f.exists() && f.isFile()) {
+				br = new BufferedReader(new FileReader(f));
+				String line = null;
+				while ((line = br.readLine()) != null) {
+					if (!line.isEmpty() && !line.trim().startsWith("#")) {
+						String t = line.trim().replaceAll("\\s+", " ");
+						if (t.contains("#")) {
+							t = t.split("#")[0];
+						}
+						String[] tokens = t.split(" ");
+						for (int i = 1; i < tokens.length; i++) {
+							System.out.println(tokens[i]);
+						}
 					}
-					String[] tokens = t.split(" ");
-					for(int i=1;i<tokens.length;i++){
-						System.out.println(tokens[i]);
-					}					
 				}
+				br.close();
 			}
-			br.close();
 			return true;
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -34,11 +37,41 @@ public class HostDiscovery {
 		return false;
 	}
 
+	public static boolean readFileSshConfig(String fileName) {
+		BufferedReader br;
+		try {
+			File f = new File(fileName);
+			if (f.exists() && f.isFile()) {
+				br = new BufferedReader(new FileReader(f));
+				String line = null;
+				while ((line = br.readLine()) != null) {
+					if (!line.isEmpty() && !line.trim().startsWith("#")) {
+						String[] l = line.trim().split(" ", 2);
+						if (l != null && l.length > 1) {
+							if (l[0].equals("Host")) {
+								System.out.println(l[1]);
+							} else if (l[0].equals("HostName")) {
+								System.out.println(l[1]);
+							}
+						}
+					}
+				}
+				br.close();
+				return true;
+			}
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		}
+		return false;
+	}
+
 	public static void main(String[] args) {
 		try {
-			readFile("/etc/hosts");
+			readFileEtcHosts("/etc/hosts");
+			readFileSshConfig(System.getProperty("user.home") + "/.ssh/config");
+			readFileSshConfig("/etc/ssh/ssh_config");
 		} catch (FileNotFoundException e) {
-			e.printStackTrace();
+			System.out.println(e.getMessage());
 		}
 	}
 }
